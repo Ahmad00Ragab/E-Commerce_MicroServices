91a628bd913f954a2cf088c77cd6ad552e9ffc45
Ahmad Haroun <Ahmad.haroun2023@gmail.com>
2024-10-18 15:46:54 +0300

    added AdminController & AdminService

---
+++ src/main/java/com/example/E_Commerce_MicroServices/controllers/AdminController.java
@@ -0,0 +1,40 @@
+package com.example.E_Commerce_MicroServices.controllers;
+
+import com.example.E_Commerce_MicroServices.models.Admin;
+import com.example.E_Commerce_MicroServices.services.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admins")
+public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @PostMapping
+    public ResponseEntity<Admin> createAdmin(@RequestBody Admin admin) {
+        Admin newAdmin = adminService.createAdmin(admin);
+        return ResponseEntity.ok(newAdmin);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Admin>> getAllAdmins() {
+        return ResponseEntity.ok(adminService.getAllAdmins());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Admin> getAdminById(@PathVariable int id) {
+        Admin admin = adminService.getAdminById(id);
+        return ResponseEntity.ok(admin);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAdmin(@PathVariable int id) {
+        adminService.deleteAdmin(id);
+        return ResponseEntity.noContent().build();
+    }
+}
