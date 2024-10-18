2c9916db80d2e0b8b9296efea7485c5eafc92539
Ahmad Haroun <Ahmad.haroun2023@gmail.com>
2024-10-18 15:06:41 +0300

    initial commit

---
+++ src/main/java/com/example/E_Commerce_MicroServices/ECommerceMicroServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.E_Commerce_MicroServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceMicroServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/E_Commerce_MicroServices/ECommerceMicroServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E_Commerce_MicroServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceMicroServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
